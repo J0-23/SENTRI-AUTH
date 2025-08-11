8c4fc048dac76034747b42171523c25392bee03d
Jorge Oliveira <jorgeoliveira2k22@gmail.com>
2025-08-11 16:40:58 +0100

    update user

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../models/auth/UserModel.js";
+
+export const protect = asyncHandler(async (req, res, next) => {
+  try {
+    // check if user is logged in
+    const token = req.cookies.token;
+
+    if (!token) {
+      // 401 unauthorized
+      res.statusCode(401).json({ message: "Not autorized, please login" });
+    }
+    // verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // get user details from token
+    const user = await User.findById(decoded.id).select("-password");
+
+    // check if user exists
+    if (!user) {
+      res.status(404).json({ message: "User not found!" });
+    }
+
+    // set user details in request object
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Not autorized, token failed" });
+  }
+});

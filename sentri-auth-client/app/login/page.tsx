993ae81979cca62c08919f0fedeace657e4f61d5
Jorge Oliveira <jorgeoliveira2k22@gmail.com>
2025-08-22 18:38:54 +0100

    frontend

---
+++ sentri-auth-client/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="auth-page w-full h-full flex justify-center items-center">
+      page
+    </div>
+  );
+};
+
+export default page;
